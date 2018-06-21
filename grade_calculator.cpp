1a9a2445f7a97368f50e5b311a0c36972c4d8b32
Johan Moon <Moon@Johans-MacBook-Pro.local>
2018-06-20 17:04:45 -0700

    changed the name of objects and implemented useSchemeA() function

--- grade_calculator.cpp
+++ grade_calculator.cpp
@@ -13,11 +13,15 @@ grade_calculator::~grade_calculator()
     delete ui;
 }
 
-//void grade_calculator::update_overall(int unused){
-//    // double score = 31.4;
-//    double score = static_cast<double>(unused);
+void grade_calculator::useSchemeA(){
 
-//    ui->label_14->setText(QString::number(score));
+    int hws = ui->hwBox->value() + ui->hwBox_2->value() + ui->hwBox_3->value() + ui->hwBox_4->value()
+                 + ui->hwBox_5->value() + ui->hwBox_6->value() + ui->hwBox_7->value() + ui->hwBox_8->value();
 
-//    return;
-//}
+    int midTerms = ui->mt1Box_9->value() + ui->mt2Box_10->value();
+
+    int final = ui->finalBox_11->value();
+
+    double total = (static_cast<double>(hws)/8) *0.25 + static_cast<double>(midTerms) * 0.40 + static_cast<double>(final) * 0.35;
+}
+
