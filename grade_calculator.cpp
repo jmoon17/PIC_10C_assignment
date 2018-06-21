7c2b4b2c546a1c20b6c1e68d34e9f004004c0513
Johan Moon <Moon@Johans-MacBook-Pro.local>
2018-06-20 17:06:10 -0700

    useSchemeB() is implemented

--- grade_calculator.cpp
+++ grade_calculator.cpp
@@ -25,3 +25,14 @@ void grade_calculator::useSchemeA(){
     double total = (static_cast<double>(hws)/8) *0.25 + static_cast<double>(midTerms) * 0.40 + static_cast<double>(final) * 0.35;
 }
 
+void grade_calculator::useSchemeB(){
+
+    int hws = ui->hwBox->value() + ui->hwBox_2->value() + ui->hwBox_3->value() + ui->hwBox_4->value()
+                 + ui->hwBox_5->value() + ui->hwBox_6->value() + ui->hwBox_7->value() + ui->hwBox_8->value();
+
+    int midTerms = ui->mt1Box_9->value() + ui->mt2Box_10->value();
+
+    int final = ui->finalBox_11->value();
+
+    double total = (static_cast<double>(hws)/8) *0.25 + static_cast<double>(midTerms) * 0.30 + static_cast<double>(final) * 0.44;
+}
