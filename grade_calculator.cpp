a4ee3a3bccaf350a2831b0b0e2b8e5f85c8b3dc2
Johan Moon <Moon@Johans-MacBook-Pro.local>
2018-06-20 17:35:43 -0700

    implemented useSchemeA and B based on courses

--- grade_calculator.cpp
+++ grade_calculator.cpp
@@ -6,6 +6,8 @@ grade_calculator::grade_calculator(QWidget *parent) :
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+
 }
 
 grade_calculator::~grade_calculator()
@@ -13,26 +15,69 @@ grade_calculator::~grade_calculator()
     delete ui;
 }
 
+//void grade_calculator::compute_overall()
+//{
+//    QString course = ui->comboBox->currentText();
+
+//    if (course == "PIC 10B Intermediate Programming"){
+//        if(useSchemeA)
+//    }
+//}
+
 void grade_calculator::useSchemeA(){
 
-    int hws = ui->hwBox->value() + ui->hwBox_2->value() + ui->hwBox_3->value() + ui->hwBox_4->value()
-                 + ui->hwBox_5->value() + ui->hwBox_6->value() + ui->hwBox_7->value() + ui->hwBox_8->value();
+    QString course = ui->comboBox->currentText();
 
-    int midTerms = ui->mt1Box_9->value() + ui->mt2Box_10->value();
+    if(course == "PIC 10B Intermediate Programming"){
+        int hws = ui->hwBox->value() + ui->hwBox_2->value() + ui->hwBox_3->value() + ui->hwBox_4->value()
+                     + ui->hwBox_5->value() + ui->hwBox_6->value() + ui->hwBox_7->value() + ui->hwBox_8->value();
 
-    int final = ui->finalBox_11->value();
+        int midTerms = ui->mt1Box_9->value() + ui->mt2Box_10->value();
 
-    double total = (static_cast<double>(hws)/8) *0.25 + static_cast<double>(midTerms) * 0.40 + static_cast<double>(final) * 0.35;
+        int final = ui->finalBox_11->value();
+
+        double total = (static_cast<double>(hws)/8) *0.25 + static_cast<double>(midTerms) * 0.40 + static_cast<double>(final) * 0.35;
+        ui->label_14->setText(total);
+    }
+
+    else if(course == "PIC 10C Intermediate Programming"){
+        int hws = ui->hwBox->value() + ui->hwBox_2->value() + ui->hwBox_3->value() + ui->hwBox_4->value()
+                     + ui->hwBox_5->value() + ui->hwBox_6->value() + ui->hwBox_7->value() + ui->hwBox_8->value();
+
+        int midTerms = ui->mt1Box_9->value() + ui->mt2Box_10->value();
+
+        int final = ui->finalBox_11->value();
+
+        double total = (static_cast<double>(hws)/8) *0.15 + static_cast<double>(midTerms) * 0.30 + static_cast<double>(final) * 0.65;
+        ui->label_14->setText(total);
+    }
 }
 
 void grade_calculator::useSchemeB(){
 
-    int hws = ui->hwBox->value() + ui->hwBox_2->value() + ui->hwBox_3->value() + ui->hwBox_4->value()
-                 + ui->hwBox_5->value() + ui->hwBox_6->value() + ui->hwBox_7->value() + ui->hwBox_8->value();
+    QString course = ui->comboBox->currentText();
+    if(course == "PIC 10B Intermediate Programming"){
+        int hws = ui->hwBox->value() + ui->hwBox_2->value() + ui->hwBox_3->value() + ui->hwBox_4->value()
+                     + ui->hwBox_5->value() + ui->hwBox_6->value() + ui->hwBox_7->value() + ui->hwBox_8->value();
 
-    int midTerms = ui->mt1Box_9->value() + ui->mt2Box_10->value();
+        int midTerms = ui->mt1Box_9->value() + ui->mt2Box_10->value();
 
-    int final = ui->finalBox_11->value();
+        int final = ui->finalBox_11->value();
 
-    double total = (static_cast<double>(hws)/8) *0.25 + static_cast<double>(midTerms) * 0.30 + static_cast<double>(final) * 0.44;
+        double total = (static_cast<double>(hws)/8) *0.25 + static_cast<double>(midTerms) * 0.30 + static_cast<double>(final) * 0.45;
+        ui->label_14->setText(total);
+    }
+
+    else if(course == "PIC 10C Intermediate Programming"){
+        int hws = ui->hwBox->value() + ui->hwBox_2->value() + ui->hwBox_3->value() + ui->hwBox_4->value()
+                     + ui->hwBox_5->value() + ui->hwBox_6->value() + ui->hwBox_7->value() + ui->hwBox_8->value();
+
+        int midTerms = ui->mt1Box_9->value() + ui->mt2Box_10->value();
+
+        int final = ui->finalBox_11->value();
+
+        double total = (static_cast<double>(hws)/8) *0.10 + static_cast<double>(midTerms) * 0.40 + static_cast<double>(final) * 0.50;
+        ui->label_14->setText(total);
+    }
+
 }
